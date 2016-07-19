cdfbb8d91053bd597276186f2681dc745d24b3dc
Todd Treece <toddtreece@gmail.com>
2016-07-19 16:02:47 -0400

    add definitions header

---
+++ AdafruitIO_Definitions.h
@@ -0,0 +1,14 @@
+#ifndef ADAFRUITIO_DEFINITIONS_H_
+#define ADAFRUITIO_DEFINITIONS_H_
+
+typedef enum {
+    AIO_IDLE                 = 0,
+    AIO_NET_DISCONNECTED     = 1,
+    AIO_NET_CONNECT_FAILED   = 2,
+    AIO_NET_CONNECTED        = 3,
+    AIO_DISCONNECTED         = 4,
+    AIO_CONNECT_FAILED       = 5,
+    AIO_CONNECTED            = 6
+} aio_status_t;
+
+#endif /* ADAFRUITIO_DEFINITIONS_H_ */
