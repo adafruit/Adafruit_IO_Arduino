2d97548c5afc62a0658cb662164f112ffa4938b6
Todd Treece <toddtreece@gmail.com>
2016-07-20 10:07:36 -0400

    remove status and mqttstatus from esp child class

--- AdafruitIO_ESP8266.cpp
+++ AdafruitIO_ESP8266.cpp
@@ -25,23 +25,6 @@ void AdafruitIO_ESP8266::_connect()
 
 }
 
-aio_status_t AdafruitIO_ESP8266::status()
-{
-
-  aio_status_t net_status = networkStatus();
-
-  // if we aren't connected, return network status
-  if(net_status != AIO_NET_CONNECTED) {
-    _status = net_status;
-    return _status;
-  }
-
-  // check mqtt status and return
-  _status = mqttStatus();
-  return _status;
-
-}
-
 aio_status_t AdafruitIO_ESP8266::networkStatus()
 {
 
@@ -54,37 +37,6 @@ aio_status_t AdafruitIO_ESP8266::networkStatus()
       return AIO_IDLE;
     default:
       return AIO_NET_DISCONNECTED;
-  }
-
-}
-
-aio_status_t AdafruitIO_ESP8266::mqttStatus()
-{
-
-  // if the connection failed,
-  // return so we don't hammer IO
-  if(_status == AIO_CONNECT_FAILED)
-    return _status;
-
-  if(_mqtt->connected())
-    return AIO_CONNECTED;
-
-  switch(_mqtt->connect(_username, _key)) {
-    case 0:
-      return AIO_CONNECTED;
-    case 1:   // invalid mqtt protocol
-    case 2:   // client id rejected
-    case 4:   // malformed user/pass
-    case 5:   // unauthorized
-    case 7:   // banned
-    case -1:  // general failure
-      return AIO_CONNECT_FAILED;
-    case 3: // mqtt service unavailable
-    case 6: // throttled
-      delay(60000); // wait 60 seconds
-      return AIO_DISCONNECTED;
-    default:
-      return AIO_DISCONNECTED;
   }
 
 }
--- AdafruitIO_ESP8266.h
+++ AdafruitIO_ESP8266.h
@@ -13,9 +13,7 @@ class AdafruitIO_ESP8266 : public AdafruitIO {
   public:
     AdafruitIO_ESP8266(const char *ssid, const char *pass);
     AdafruitIO_ESP8266(const __FlashStringHelper *ssid, const __FlashStringHelper *pass);
-    aio_status_t status();
     aio_status_t networkStatus();
-    aio_status_t mqttStatus();
 
   protected:
     void _connect();
