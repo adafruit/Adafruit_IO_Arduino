84c1faa45148614fe0ceb141c8dbbc005cd51b37
Todd Treece <toddtreece@gmail.com>
2016-08-02 11:03:23 -0400

    add wiced wrapper

---
+++ AdafruitIO_WICED.cpp
@@ -0,0 +1,41 @@
+#if defined(STM32_MEDIUM_DENSITY) || defined(STM32_HIGH_DENSITY)
+
+#include "AdafruitIO_WICED.h"
+
+AdafruitIO_WICED::AdafruitIO_WICED(const char *ssid, const char *pass):AdafruitIO()
+{
+  _ssid = ssid;
+  _pass = pass;
+  _client = new AdafruitTCP;
+  _mqtt = new Adafruit_MQTT_Client(_client, _host, _port);
+}
+
+AdafruitIO_WICED::AdafruitIO_WICED(const __FlashStringHelper *ssid, const __FlashStringHelper *pass):AdafruitIO()
+{
+  _ssid = (const char*)ssid;
+  _pass = (const char*)pass;
+  _client = new AdafruitTCP;
+  _mqtt = new Adafruit_MQTT_Client(_client, _host, _port);
+}
+
+void AdafruitIO_WICED::_connect()
+{
+  Feather.connect(_ssid, _pass);
+  _status = AIO_NET_DISCONNECTED;
+}
+
+aio_status_t AdafruitIO_WICED::networkStatus()
+{
+  if(Feather.connected())
+    return AIO_NET_CONNECTED;
+
+  // if granular status is needed, we can
+  // check Feather.errno() codes:
+  // https://learn.adafruit.com/introducing-the-adafruit-wiced-feather-wifi/constants#err-t
+  // for now we will try connecting again and return disconnected status
+  Feather.connect(_ssid, _pass);
+  return AIO_NET_DISCONNECTED;
+}
+
+
+#endif // STM32
---
+++ AdafruitIO_WICED.h
@@ -0,0 +1,29 @@
+#ifndef ADAFRUITIO_WICED_H
+#define ADAFRUITIO_WICED_H
+
+#if defined(STM32_MEDIUM_DENSITY) || defined(STM32_HIGH_DENSITY)
+
+#include "Arduino.h"
+#include "AdafruitIO.h"
+#include <adafruit_feather.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+class AdafruitIO_WICED : public AdafruitIO {
+
+  public:
+    AdafruitIO_WICED(const char *ssid, const char *pass);
+    AdafruitIO_WICED(const __FlashStringHelper *ssid, const __FlashStringHelper *pass);
+    aio_status_t networkStatus();
+
+  protected:
+    void _connect();
+    const char *_ssid;
+    const char *_pass;
+    AdafruitTCP *_client;
+
+};
+
+#endif // STM32
+
+#endif // ADAFRUITIO_WICED_H
