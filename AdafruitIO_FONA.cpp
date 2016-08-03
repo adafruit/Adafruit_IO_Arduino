653566199164c7fbcbc91eadfd84e2a89029b930
Todd Treece <toddtreece@gmail.com>
2016-08-03 17:28:45 -0400

    add fona class

---
+++ AdafruitIO_FONA.cpp
@@ -0,0 +1,67 @@
+#ifdef ARDUINO_ARCH_AVR
+
+#include "AdafruitIO_FONA.h"
+
+AdafruitIO_FONA::AdafruitIO_FONA(const char *apn, const char *apn_user, const char *apn_pass):AdafruitIO()
+{
+  _apn = apn;
+  _apn_user = apn_user;
+  _apn_pass = apn_pass;
+  _serial = new SoftwareSerial(FONA_TX, FONA_RX);
+  _fona = new Adafruit_FONA(FONA_RST);
+  _mqtt = new Adafruit_MQTT_FONA(_fona, _host, _port);
+}
+
+AdafruitIO_FONA::AdafruitIO_FONA(const __FlashStringHelper *apn, const __FlashStringHelper *apn_user, const __FlashStringHelper *apn_pass):AdafruitIO()
+{
+  _apn = (const char*)apn;
+  _apn_user = (const char*)apn_user;
+  _apn_pass = (const char*)apn_pass;
+  _serial = new SoftwareSerial(FONA_TX, FONA_RX);
+  _fona = new Adafruit_FONA(FONA_RST);
+  _mqtt = new Adafruit_MQTT_FONA(_fona, _host, _port);
+}
+
+void AdafruitIO_FONA::_connect()
+{
+
+  // set software serial baud rate
+  _serial->begin(FONA_BAUD);
+
+  // if fona can't be found, bail
+  if(! _fona->begin(*_serial)) {
+    _status = AIO_NET_CONNECT_FAILED;
+    return;
+  }
+
+  // disable cme error reporting
+  _serial->println("AT+CMEE=2");
+
+  _status = AIO_NET_DISCONNECTED;
+
+}
+
+aio_status_t AdafruitIO_FONA::networkStatus()
+{
+
+  // return if in a failed state
+  if(_status == AIO_NET_CONNECT_FAILED)
+    return _status;
+
+  // if we are connected, return
+  if(_fona->GPRSstate())
+   return AIO_NET_CONNECTED;
+
+  // wait for connection to network
+  if(_fona->getNetworkStatus() != 1)
+    return AIO_NET_DISCONNECTED;
+
+  // set apn info
+  // _fona->setGPRSNetworkSettings(_apn, _apn_user, _apn_pass);
+
+  _fona->enableGPRS(true);
+  return AIO_NET_CONNECTED;
+
+}
+
+#endif // ARDUINO_ARCH_AVR
---
+++ AdafruitIO_FONA.h
@@ -0,0 +1,42 @@
+#ifndef ADAFRUITIO_FONA_H
+#define ADAFRUITIO_FONA_H
+
+#ifdef ARDUINO_ARCH_AVR
+
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+#include "Adafruit_FONA.h"
+#include "AdafruitIO.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_FONA.h"
+
+#define FONA_RX  9
+#define FONA_TX  8
+#define FONA_RST 4
+#define FONA_RI  7
+#define FONA_BAUD 4800
+
+class AdafruitIO_FONA : public AdafruitIO {
+
+  public:
+    AdafruitIO_FONA(const char *apn, const char *apn_user, const char *apn_pass);
+    AdafruitIO_FONA(const __FlashStringHelper *apn, const __FlashStringHelper *apn_user, const __FlashStringHelper *apn_pass);
+    aio_status_t networkStatus();
+
+  protected:
+    void _connect();
+
+    const char *_apn;
+    const char *_apn_user;
+    const char *_apn_pass;
+
+    uint16_t _port = 1883;
+
+    SoftwareSerial *_serial;
+    Adafruit_FONA *_fona;
+
+};
+
+#endif // ARDUINO_ARCH_AVR
+
+#endif // ADAFRUITIO_FONA_H
