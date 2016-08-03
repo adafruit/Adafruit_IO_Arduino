e09935d8500b513272499cba9c2486f85c36c9aa
Todd Treece <toddtreece@gmail.com>
2016-08-03 10:04:01 -0400

    add new status codes to definition header

--- AdafruitIO_Definitions.h
+++ AdafruitIO_Definitions.h
@@ -19,15 +19,26 @@ typedef void (*AdafruitIODataCallbackType)(AdafruitIO_Data *data);
 #define AIO_SSL_FINGERPRINT "26 96 1C 2A 51 07 FD 15 80 96 93 AE F7 32 CE B9 0D 01 55 C4"
 
 typedef enum {
-    AIO_IDLE                 = 0,
-    AIO_NET_DISCONNECTED     = 1,
-    AIO_NET_CONNECT_FAILED   = 2,
-    AIO_NET_CONNECTED        = 3,
-    AIO_DISCONNECTED         = 4,
-    AIO_CONNECT_FAILED       = 5,
-    AIO_CONNECTED            = 6,
-    AIO_SSL_UNVERIFIED       = 7,
-    AIO_SSL_VERIFIED         = 8
+
+    // CONNECTING
+    AIO_IDLE                    = 0,
+    AIO_NET_DISCONNECTED        = 1,
+    AIO_DISCONNECTED            = 2,
+    AIO_FINGERPRINT_UNKOWN      = 3,
+
+    // FAILURE
+    AIO_NET_CONNECT_FAILED      = 10,
+    AIO_CONNECT_FAILED          = 11,
+    AIO_FINGERPRINT_INVALID     = 12,
+    AIO_AUTH_FAILED             = 13,
+
+    // SUCCESS
+    AIO_NET_CONNECTED           = 20,
+    AIO_CONNECTED               = 21,
+    AIO_CONNECTED_INSECURE      = 22,
+    AIO_FINGERPRINT_UNSUPPORTED = 23,
+    AIO_FINGERPRINT_VALID       = 24
+
 } aio_status_t;
 
 #endif /* ADAFRUITIO_DEFINITIONS_H_ */
