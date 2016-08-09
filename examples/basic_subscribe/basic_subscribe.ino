3eb8d15b032293200c4fa66ac8eeb2dca8cae51b
Todd Treece <toddtreece@gmail.com>
2016-08-09 10:44:59 -0400

    add basic_subscribe example

---
+++ examples/basic_subscribe/basic_subscribe.ino
@@ -0,0 +1,109 @@
+//
+//      Adafruit IO Basic Subscription Example
+//
+//                        ▄████
+//                      ▄███████
+//                     █████████▌
+//                    ███████████
+//                   ████████████▌
+//  ███████████████▄ ████████████▌
+//   █████████████████████▀▀█████ ▄▄▄▄▄▄▄
+//    ▐██████████████████   █████████████████▄▄
+//      ▀█████████  ▀▀███  ██████████████████████
+//        █████████▄▄  ▐████▀    ▐█████████████▀
+//          ▀▀███████████████▄▄█████████████▀
+//           ▄███████   ██  ▀████████████▀
+//          ███████▀  ▄████  ▐█████▄
+//         █████████████████▄▄██████▄
+//        ███████████████████████████
+//       ██████████████ ▐████████████▌
+//      ▐██████████▀▀    ▀███████████▌
+//      █████▀▀            ▀█████████▌
+//                            ▀██████
+//                               ▀███
+//
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+//
+
+/************************* WiFi Access Point *********************************/
+
+#define WIFI_SSID       "your_ssid"
+#define WIFI_PASS       "your_pass"
+
+/************************* Adafruit IO Setup ********************************/
+
+#define IO_USERNAME    "your_username"
+#define IO_KEY         "your_key"
+
+/*************************** Client Setup ***********************************/
+
+// The Adafruit IO WiFi client will work with the following boards:
+//   - HUZZAH ESP8266 Breakout -> https://www.adafruit.com/products/2471
+//   - Feather HUZZAH ESP8266 -> https://www.adafruit.com/products/2821
+//   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
+//   - Feather WICED -> https://www.adafruit.com/products/3056
+
+// set up the wifi client using the supplied ssid & pass:
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(WIFI_SSID, WIFI_PASS);
+
+/************************ Example Starts Here *******************************/
+
+// set up the 'counter' feed
+AdafruitIO_Feed *counter = io.feed("counter");
+
+void setup() {
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  Serial.print("Connecting to Adafruit IO");
+
+  // connect to io.adafruit.com
+  io.connect(IO_USERNAME, IO_KEY);
+
+  // set up a message handler for the count feed.
+  // the handleMessage function (defined below)
+  // will be called whenever a message is
+  // received from adafruit io.
+  counter->onMessage(handleMessage);
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+}
+
+// this function is called whenever a 'counter' message
+// is received from Adafruit IO. it was attached to
+// the counter feed in the setup() function above.
+void handleMessage(AdafruitIO_Data *data) {
+
+  Serial.print("received <- ");
+  Serial.println(data->value());
+
+}
