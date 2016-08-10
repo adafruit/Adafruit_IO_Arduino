43f3a9629f1e7c06872b467a04022eae3abecb64
Todd Treece <toddtreece@gmail.com>
2016-08-10 11:19:00 -0400

    add ethernet class

---
+++ AdafruitIO_Ethernet.h
@@ -0,0 +1,49 @@
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Copyright (c) 2015-2016 Adafruit Industries
+// Authors: Tony DiCola, Todd Treece
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+//
+#ifndef ADAFRUITIO_Ethernet_H
+#define ADAFRUITIO_Ethernet_H
+
+#if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_SAMD)
+
+#include "Arduino.h"
+#include <SPI.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#include <Ethernet2.h>
+#include <EthernetClient.h>
+#include <Dns.h>
+#include <Dhcp.h>
+
+#include "AdafruitIO.h"
+
+class AdafruitIO_Ethernet : public AdafruitIO {
+
+  public:
+    AdafruitIO_Ethernet(const char *user, const char *key);
+    AdafruitIO_Ethernet(const __FlashStringHelper *user, const __FlashStringHelper *key);
+
+    aio_status_t networkStatus();
+
+  protected:
+    void _connect();
+
+    byte _mac[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+    uint16_t _port = 1883;
+
+    EthernetClient *_client;
+
+};
+
+#endif // ARDUINO_ARCH_AVR || ARDUINO_ARCH_SAMD
+
+#endif // ADAFRUITIO_Ethernet_H
