f18f50002025d90d7e4c92d3a2b47237c99e6ef7
Todd Treece <toddtreece@gmail.com>
2016-08-12 17:08:29 -0400

    add reworked digital out sketch

---
+++ examples/adafruitio_07_digital_out/adafruitio_07_digital_out.ino
@@ -0,0 +1,67 @@
+// Adafruit IO Digital Output Example
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+
+/************************ Example Starts Here *******************************/
+
+// digital pin 5
+#define LED_PIN 5
+
+// set up the 'led' feed
+AdafruitIO_Feed *led = io.feed("led");
+
+void setup() {
+
+  // set led pin as a digital output
+  pinMode(LED_PIN, OUTPUT);
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+  // write the current state to the led
+  digitalWrite(LED_PIN, led->data->toPinLevel());
+
+}
+
