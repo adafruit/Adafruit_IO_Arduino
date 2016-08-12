760e5c8c16da4288aaf2d972b835b1195a791fd5
Todd Treece <toddtreece@gmail.com>
2016-08-12 14:11:44 -0400

    move fona logic to .h

--- AdafruitIO_FONA.h
+++ AdafruitIO_FONA.h
@@ -12,8 +12,6 @@
 #ifndef ADAFRUITIO_FONA_H
 #define ADAFRUITIO_FONA_H
 
-#ifdef ARDUINO_ARCH_AVR
-
 #include "Arduino.h"
 #include <SoftwareSerial.h>
 #include "Adafruit_MQTT.h"
@@ -27,26 +25,72 @@
 #define FONA_RI  7
 #define FONA_BAUD 4800
 
+// all logic in .h to avoid auto compile
 class AdafruitIO_FONA : public AdafruitIO {
 
   public:
-    AdafruitIO_FONA(const char *user, const char *key);
-    AdafruitIO_FONA(const __FlashStringHelper *user, const __FlashStringHelper *key);
+    AdafruitIO_FONA(const char *user, const char *key):AdafruitIO(user, key)
+    {
+      _serial = new SoftwareSerial(FONA_TX, FONA_RX);
+      _fona = new Adafruit_FONA(FONA_RST);
+      _mqtt = new Adafruit_MQTT_FONA(_fona, _host, _port);
+      _packetread_timeout = 500;
+    }
 
-    void setAPN(FONAFlashStringPtr apn, FONAFlashStringPtr username=0, FONAFlashStringPtr password=0);
+    AdafruitIO_FONA(const __FlashStringHelper *user, const __FlashStringHelper *key):AdafruitIO(user, key)
+    {
+      _serial = new SoftwareSerial(FONA_TX, FONA_RX);
+      _fona = new Adafruit_FONA(FONA_RST);
+      _mqtt = new Adafruit_MQTT_FONA(_fona, _host, _port);
+      _packetread_timeout = 500;
+    }
 
-    aio_status_t networkStatus();
+    void setAPN(FONAFlashStringPtr apn, FONAFlashStringPtr username=0, FONAFlashStringPtr password=0)
+    {
+      _fona->setGPRSNetworkSettings(apn, username, password);
+    }
+
+    aio_status_t AdafruitIO_FONA::networkStatus()
+    {
+      // return if in a failed state
+      if(_status == AIO_NET_CONNECT_FAILED)
+        return _status;
+
+      // if we are connected, return
+      if(_fona->GPRSstate())
+       return AIO_NET_CONNECTED;
+
+      // wait for connection to network
+      if(_fona->getNetworkStatus() != 1)
+        return AIO_NET_DISCONNECTED;
+
+      _fona->enableGPRS(true);
+      return AIO_NET_CONNECTED;
+    }
 
   protected:
-    void _connect();
-
     uint16_t _port = 1883;
 
     SoftwareSerial *_serial;
     Adafruit_FONA *_fona;
 
-};
+    void _connect()
+    {
+      // set software serial baud rate
+      _serial->begin(FONA_BAUD);
 
-#endif // ARDUINO_ARCH_AVR
+      // if fona can't be found, bail
+      if(! _fona->begin(*_serial)) {
+        _status = AIO_NET_CONNECT_FAILED;
+        return;
+      }
+
+      // disable cme error reporting
+      _serial->println("AT+CMEE=2");
+
+      _status = AIO_NET_DISCONNECTED;
+    }
+
+};
 
 #endif // ADAFRUITIO_FONA_H
