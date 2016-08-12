d23b80e4799ec51acc4f8d2a225b5681fad0bd21
Todd Treece <toddtreece@gmail.com>
2016-08-12 16:30:42 -0400

    add reworked digital in example

---
+++ examples/adafruitio_06_digital_in/adafruitio_06_digital_in.ino
@@ -0,0 +1,87 @@
+// Adafruit IO Digital Input Example
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Written by Todd Treece for Adafruit Industries
+// Copyright (c) 2016 Adafruit Industries
+// Licensed under the MIT license.
+//
+// All text above must be included in any redistribution.
+
+/************************** Configuration ***********************************/
+
+// edit the config.h tab and enter your Adafruit IO credentials
+// and any additional configuration needed for WiFi, cellular,
+// or ethernet clients.
+#include "config.h"
+
+/************************ Example Starts Here *******************************/
+
+// digital pin 5
+#define BUTTON_PIN 5
+
+// button state
+bool current = false;
+bool last = false;
+
+// set up the 'button' feed
+AdafruitIO_Feed *button = io.feed("button");
+
+void setup() {
+
+  // set button pin as an input
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // start the serial connection
+  Serial.begin(115200);
+
+  // wait for serial monitor to open
+  while(! Serial);
+
+  // connect to io.adafruit.com
+  Serial.print("Connecting to Adafruit IO");
+  io.connect();
+
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  // we are connected
+  Serial.println();
+  Serial.println(io.statusText());
+
+}
+
+void loop() {
+
+  // io.run(); is required for all sketches.
+  // it should always be present at the top of your loop
+  // function. it keeps the client connected to
+  // io.adafruit.com, and processes any incoming data.
+  io.run();
+
+  // grab the current state of the button.
+  // we have to flip the logic because we are
+  // using INPUT_PULLUP.
+  if(digitalRead(BUTTON_PIN) == LOW)
+    current = true;
+  else
+    current = false;
+
+  // return if the value hasn't changed
+  if(current == last)
+    return;
+
+  // save the current state to the button feed
+  Serial.print("sending button -> ");
+  Serial.println(current);
+  button->save(current);
+
+  // store last button state
+  last = current;
+
+}
