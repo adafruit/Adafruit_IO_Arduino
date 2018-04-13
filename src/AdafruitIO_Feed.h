4bb7395d4e89601341ab894b653aabcb8b9f6c77
Adam Bachman <adam.bachman@gmail.com>
2018-04-13 15:30:38 -0400

    add /get MQTT publish method to trigger feed data resending

--- src/AdafruitIO_Feed.h
+++ src/AdafruitIO_Feed.h
@@ -37,6 +37,8 @@ class AdafruitIO_Feed : public AdafruitIO_MQTT {
     bool save(float value, double lat=0, double lon=0, double ele=0, int precision=6);
     bool save(double value, double lat=0, double lon=0, double ele=0, int precision=6);
 
+    bool get();
+
     bool exists();
     bool create();
 
@@ -56,11 +58,13 @@ class AdafruitIO_Feed : public AdafruitIO_MQTT {
     void _init();
 
     char *_topic;
+    char *_get_topic;
     char *_create_url;
     char *_feed_url;
 
     Adafruit_MQTT_Subscribe *_sub;
     Adafruit_MQTT_Publish *_pub;
+    Adafruit_MQTT_Publish *_get_pub;
 
     AdafruitIO *_io;
     AdafruitIO_Data *_data;
