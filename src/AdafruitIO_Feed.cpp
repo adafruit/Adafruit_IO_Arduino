48c3906a4f31e44de7de6df502b13e6059ba1740
Imre Horvath <imi.horvath@gmail.com>
2018-05-27 14:20:27 +0200

    Follow up the Yun FW release changes

--- src/AdafruitIO_Feed.cpp
+++ src/AdafruitIO_Feed.cpp
@@ -213,6 +213,25 @@ AdafruitIO_Data* AdafruitIO_Feed::lastValue()
   AIO_DEBUG_PRINT("lastValue get ");
   AIO_DEBUG_PRINTLN(url);
 
+#if defined(ARDUINO_AVR_YUN)
+  String aiokey = "X-AIO-Key: ";
+  aiokey += _io->_key;
+
+  _io->_http->startRequest();
+  _io->_http->addHeader(aiokey.c_str());
+  _io->_http->enableInsecure();
+  _io->_http->get(url.c_str());
+
+  int status = _io->_http->getResponseCode();
+
+  // Collect the response body into this string
+  String body;
+
+  while (_io->_http->available() > 0) {
+    char c = _io->_http->read();
+    body += c;
+  }
+#else
   _io->_http->beginRequest();
   _io->_http->get(url.c_str());
   _io->_http->sendHeader("X-AIO-Key", _io->_key);
@@ -220,6 +239,7 @@ AdafruitIO_Data* AdafruitIO_Feed::lastValue()
 
   int status = _io->_http->responseStatusCode();
   String body = _io->_http->responseBody();
+#endif // defined(ARDUINO_AVR_YUN)
 
   if (status >= 200 && status <= 299) {
 
@@ -232,7 +252,7 @@ AdafruitIO_Data* AdafruitIO_Feed::lastValue()
     AIO_ERROR_PRINT("error retrieving lastValue, status: ");
     AIO_ERROR_PRINTLN(status);
     AIO_ERROR_PRINT("response body: ");
-    AIO_ERROR_PRINTLN(_io->_http->responseBody());
+    AIO_ERROR_PRINTLN(body);
 
     return NULL;
 
