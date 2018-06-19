835b840772f010b12068452c47a52180b497fb61
brentrubell <brentru@users.noreply.github.com>
2018-06-19 11:20:28 -0400

    fix-example-07-esp32

--- examples/adafruitio_07_digital_out/adafruitio_07_digital_out.ino
+++ examples/adafruitio_07_digital_out/adafruitio_07_digital_out.ino
@@ -27,10 +27,9 @@
 AdafruitIO_Feed *digital = io.feed("digital");
 
 void setup() {
-
-  // set led pin as a digital output
+  
   pinMode(LED_PIN, OUTPUT);
-
+  
   // start the serial connection
   Serial.begin(115200);
 
@@ -56,7 +55,6 @@ void setup() {
   // we are connected
   Serial.println();
   Serial.println(io.statusText());
-  digital->get();
 
 }
 
@@ -82,7 +80,6 @@ void handleMessage(AdafruitIO_Data *data) {
   else
     Serial.println("LOW");
 
-  // write the current state to the led
-  digitalWrite(LED_PIN, data->toPinLevel());
 
+  digitalWrite(LED_PIN, data->toPinLevel());
 }
