658899dd73f8d9326dca2ff651cf7dedfa30d5e7
brentru <robots199Wme.com>
2019-06-20 16:53:26 -0400

    modifying examples to maintain nina pin standard

--- examples/io_home_series/neopixel_and_thermometer/config.h
+++ examples/io_home_series/neopixel_and_thermometer/config.h
@@ -2,8 +2,8 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME    "your_username"
-#define IO_KEY         "your_key"
+#define IO_USERNAME   "your_username"
+#define IO_KEY        "your_key"
 
 /******************************* WIFI **************************************/
 
@@ -13,15 +13,38 @@
 //   - Feather HUZZAH ESP32 -> https://www.adafruit.com/product/3405
 //   - Feather M0 WiFi -> https://www.adafruit.com/products/3010
 //   - Feather WICED -> https://www.adafruit.com/products/3056
+//   - Adafruit PyPortal -> https://www.adafruit.com/product/4116
+//   - Adafruit Metro M4 Express AirLift Lite -> https://www.adafruit.com/product/4000
+//   - Adafruit AirLift Breakout -> https://www.adafruit.com/product/4201
+//   - Adafruit AirLift Shield -> https://www.adafruit.com/product/4285
+//   - Adafruit AirLift FeatherWing -> https://www.adafruit.com/product/4264
 
-#define WIFI_SSID       "your_ssid"
-#define WIFI_PASS       "your_pass"
+#define WIFI_SSID   "your_ssid"
+#define WIFI_PASS   "your_pass"
 
-// comment out the following two lines if you are using fona or ethernet
+// uncomment the following line if you are using airlift
+// #define USE_AIRLIFT
+
+// uncomment the following line if you are using winc1500
+// #define USE_WINC1500
+
+// comment out the following lines if you are using fona or ethernet
 #include "AdafruitIO_WiFi.h"
-AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
-
+#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE)
+  // Configure the pins used for the ESP32 connection
+  #if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
+    // Don't change the names of these #define's! they match the variant ones
+    #define SPIWIFI SPI
+    #define SPIWIFI_SS 10  // Chip select pin
+    #define NINA_ACK 9    // a.k.a BUSY or READY pin
+    #define NINA_RESETN 6 // Reset pin
+    #define NINA_GPIO0 -1 // Not connected
+  #endif
+  AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS, NINA_ACK, NINA_RESETN, NINA_GPIO0, &SPIWIFI);
+#else
+  AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+#endif
 /******************************* FONA **************************************/
 
 // the AdafruitIO_FONA client will work with the following boards:
@@ -31,7 +54,6 @@ AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 // and comment out the AdafruitIO_WiFi client in the WIFI section
 // #include "AdafruitIO_FONA.h"
 // AdafruitIO_FONA io(IO_USERNAME, IO_KEY);
-
 
 /**************************** ETHERNET ************************************/
 
