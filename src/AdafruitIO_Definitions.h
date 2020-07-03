14c311d3015b2563faf2a6904128012b9019e427
Zach Auclair <zach101@gmail.com>
2020-07-03 17:57:14 -0400

    fix(definitions): feed name incorrectly small
    Grouped feeds can be a max of 258 characters long (128 for the group, a single dot,
    and another 128 for the feed name).

--- src/AdafruitIO_Definitions.h
+++ src/AdafruitIO_Definitions.h
@@ -124,7 +124,10 @@ public:
                                                                 ///< SSL
                                                                 ///< Fingerprint
 
-#define AIO_FEED_NAME_LENGTH 20 ///< Maximum length of an Adafruit IO Feed name
+#define AIO_FEED_NAME_LENGTH                                                   \
+  258 ///< Maximum length of an Adafruit IO Feed    \
+                                  ///< Name; 128 + 1 + 128 for the group, a dot \
+                                  ///< , and actual feed name.
 #define AIO_DATA_LENGTH                                                        \
   45 ///< Maximum length of data sent/recieved from Adafruit IO
 #define AIO_CSV_LENGTH                                                         \
