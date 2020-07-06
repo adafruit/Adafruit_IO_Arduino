70c19d4b6bf058ff35504e2e2cbaa01945d76f81
Doxygen CI <travis@travis-ci.org>
2020-07-06 20:53:26 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_e.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['run',['run',['../class_adafruit_i_o.html#a1006551006e1c780a67d36903bf9332d',1,'AdafruitIO']]]
-];
--- html/search/functions_b.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['networkstatus',['networkStatus',['../class_adafruit_i_o.html#a80f3b21779818214d21264b8790f9c7f',1,'AdafruitIO::networkStatus()'],['../class_adafruit_i_o___ethernet.html#aad21e9b8dfc58c9d24e53b16c0f61d04',1,'AdafruitIO_Ethernet::networkStatus()'],['../class_adafruit_i_o___f_o_n_a.html#a0406bf1046b7fe99322fa5d9407d2daf',1,'AdafruitIO_FONA::networkStatus()']]]
-];
