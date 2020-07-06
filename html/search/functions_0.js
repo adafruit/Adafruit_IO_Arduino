79a7e9f1b7f2be352ae6c60578f3a24661cdcee0
Doxygen CI <travis@travis-ci.org>
2020-07-06 20:48:13 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

---
+++ html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['_5fconnect',['_connect',['../class_adafruit_i_o.html#a335e7adc061806aecb8c5e0280dbcf8d',1,'AdafruitIO::_connect()'],['../class_adafruit_i_o___ethernet.html#a36d6a67a38a699b765136d0491c5ce64',1,'AdafruitIO_Ethernet::_connect()'],['../class_adafruit_i_o___f_o_n_a.html#a726026d0da43d348fb93fc8491d29342',1,'AdafruitIO_FONA::_connect()']]],
+  ['_5fdisconnect',['_disconnect',['../class_adafruit_i_o.html#a003a0d33ee2b208e6bfedf0bafa5e5ce',1,'AdafruitIO::_disconnect()'],['../class_adafruit_i_o___ethernet.html#a051677b1215824eb0505f1a711d553fb',1,'AdafruitIO_Ethernet::_disconnect()']]]
+];
