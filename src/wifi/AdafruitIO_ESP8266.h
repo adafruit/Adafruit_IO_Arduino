bf9c3392a33043e7f3c4693fc2a7e52fe6e5253c
brentru <brent@adafruit.com>
2022-08-09 10:32:39 -0400

    comments

--- src/wifi/AdafruitIO_ESP8266.h
+++ src/wifi/AdafruitIO_ESP8266.h
@@ -22,6 +22,17 @@
 #include "Adafruit_MQTT_Client.h"
 #include "Arduino.h"
 #include "ESP8266WiFi.h"
+/* NOTE - Projects that require "Secure MQTT" (TLS/SSL) also require a new
+ * SSL certificate every year. If adding Secure MQTT to your ESP8266 project is
+ * important  - please switch to using the modern ESP32 (and related models)
+ * instead of the ESP8266 to avoid updating the SSL fingerprint every year.
+ *
+ * If you've read through this and still want to use "Secure MQTT" with your
+ * ESP8266 project, we've left the "WiFiClientSecure" lines commented out. To
+ * use them, uncomment the commented out lines within `AdafruitIO_ESP8266.h` and
+ * `AdafruitIO_ESP8266.cpp` and recompile the library.
+ */
+// #include "WiFiClientSecure.h"
 
 class AdafruitIO_ESP8266 : public AdafruitIO {
 
@@ -40,6 +51,9 @@ protected:
   const char *_ssid;
   const char *_pass;
   WiFiClient *_client;
+  // Uncomment the following line, and remove the line above, to use
+  // secure MQTT with ESP8266.
+  // WiFiClientSecure *_client;
 };
 
 #endif // ESP8266
